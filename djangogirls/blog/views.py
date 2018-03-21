9216fc4471e3ddf4cfac2f0d9633b38458150c16
Luis <happystep@ksu.edu>
2018-03-21 17:52:08 -0500

    changes, added widgit but it don't find pattern

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -11,3 +11,6 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def chat(request):
+    return render(request, 'blog/chat.html', {})
